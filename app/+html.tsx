e2dd01ba6da586608d01cc0c2db9feab39b4925a
Graham Dee <grahamdee@Grahams-MacBook-Pro.local>
2026-04-22 14:01:19 -0700

    Add apple-touch-icon for iOS home screen

--- app/+html.tsx
+++ app/+html.tsx
@@ -12,6 +12,7 @@ export default function Root({ children }: { children: React.ReactNode }) {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png" />
 
         {/* 
           Disable body scrolling on web. This makes ScrollView components work closer to how they do on native. 
